cfa72b09ec48adf390a76d76c24c753ae3faf67b
Yu <yuzhao88@gmail.com>
2014-12-14 17:37:51 -0800

    remove

--- .idea/ilp.py
+++
@@ -1,7 +0,0 @@
-from pivot import *
-from Optimize import *
-
-fileName = "C:\Users\Yu Zhao\PycharmProjects\untitled\ilpTests\ilpTests\unitTests\ilpTest0"
-
-bv, nbv, b,A,z = getVariables(fileName)
-bvFinal, nbvFinal, bFinal, AFinal, zFinal = init(bv, nbv, b, A, z)
